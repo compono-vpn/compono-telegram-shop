3934a5d653f967693b7801b4b9edcdd89984ed7e
Anton Iusupov <antonyusupov@outlook.com>
2026-03-14 19:12:11 +0300

    Fix source prefix to use hyphen (source-) matching deep link format

--- src/core/constants.py
+++ src/core/constants.py
@@ -27,7 +27,7 @@ PURCHASE_PREFIX: Final[str] = "purchase_"
 GOTO_PREFIX: Final[str] = "gt_"
 ENCRYPTED_PREFIX: Final[str] = "enc_"
 REFERRAL_PREFIX: Final[str] = "ref_"
-SOURCE_PREFIX: Final[str] = "source_"
+SOURCE_PREFIX: Final[str] = "source-"
 
 IMPORTED_TAG: Final[str] = "IMPORTED"
 
